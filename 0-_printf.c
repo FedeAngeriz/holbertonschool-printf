4d94176a8c91ed5735e62bdfebd6b23f61a71953
FedeAngeriz <10086@holbertonstudents.com>
2024-11-26 20:50:01 +0000

    Prueba repo

---
+++ 0-_printf.c
@@ -0,0 +1,15 @@
+#include "printf.h"
+
+int _printf(const char *format, ...)
+{
+	unsigned int i;
+
+
+	if (format == NULL)
+		return (-1);
+
+	for (i = 0; i < '\0'; i++)
+	{
+
+	}
+}
---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+
+#endif
